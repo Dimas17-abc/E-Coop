df819c207457c58640b7a4e3523671c54ec80a2e
Dimas Fadilah Fatuhrahman <dim4as54@gmail.com>
2024-03-21 09:26:53 +0700

    belajar routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-name', function() {
+    echo "Hallo orang baik";
+});
+Route::get('/my-address/{param}', function($address) {
+    echo "Alamat di " . $address;
+});
+
+// student
+
+Route::get('/get-student/{name}/{code}', function($name, $code){
+    echo "nama saya " . $name , " NRP: " .$code;
+});
